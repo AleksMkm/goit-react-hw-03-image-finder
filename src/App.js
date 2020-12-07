ffaf68712cc85bda34f28b93a85b7770d03b6bcd
Aleksandr Mukomelov <mukomelov.aleksandr@gmail.com>
2020-12-08 01:05:28 +0200

    init: initializes project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Container from './Components/Container';
+import Section from './Components/Section';
+
+function App() {
+  return (
+    <Container>
+      <Section title="hello there" />
+    </Container>
+  );
+}
+
+export default App;
