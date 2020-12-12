f34a007dc9a4f62d17698926567db9776ee03edf
Aleksandr Mukomelov <mukomelov.aleksandr@gmail.com>
2020-12-12 12:06:28 +0200

    feat: images loading is now progressive & lazy. dependancy: react-lazy-load, react-progressive-image

--- src/Components/ImageGallery/ImageGalleryItem/ImageGalleryItem.js
+++ src/Components/ImageGallery/ImageGalleryItem/ImageGalleryItem.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import LazyLoad from 'react-lazy-load';
+import ProgressiveImage from 'react-progressive-image';
 import s from './ImageGalleryItem.module.css';
 
 function ImageGalleryItem({ image }) {
   return (
     <li className={s.item}>
-      <img
-        src={image.webformatURL}
-        alt={image.tags}
-        data-image={image.largeImageURL}
-        className={s.image}
-      />
+      <LazyLoad height={260}>
+        <ProgressiveImage
+          src={image.webformatURL}
+          placeholder={image.previewURL}
+        >
+          {(src, loading) => (
+            <img
+              style={{ opacity: loading ? 0.5 : 1 }}
+              src={src}
+              alt={image.tags}
+              data-image={image.largeImageURL}
+              className={s.image}
+            />
+          )}
+        </ProgressiveImage>
+      </LazyLoad>
     </li>
   );
 }
