cfbf517f4e9cf60710297e038f74a22213970e1d
Aleksandr Mukomelov <mukomelov.aleksandr@gmail.com>
2020-12-12 13:01:36 +0200

    fix: lazy load removed, library depreciated. dependancy: reat-lazy-load removed

--- src/Components/ImageGallery/ImageGalleryItem/ImageGalleryItem.js
+++ src/Components/ImageGallery/ImageGalleryItem/ImageGalleryItem.js
@@ -1,27 +1,21 @@
 import React from 'react';
-import LazyLoad from 'react-lazy-load';
 import ProgressiveImage from 'react-progressive-image';
 import s from './ImageGalleryItem.module.css';
 
 function ImageGalleryItem({ image }) {
   return (
     <li className={s.item}>
-      <LazyLoad height={260}>
-        <ProgressiveImage
-          src={image.webformatURL}
-          placeholder={image.previewURL}
-        >
-          {(src, loading) => (
-            <img
-              style={{ opacity: loading ? 0.5 : 1 }}
-              src={src}
-              alt={image.tags}
-              data-image={image.largeImageURL}
-              className={s.image}
-            />
-          )}
-        </ProgressiveImage>
-      </LazyLoad>
+      <ProgressiveImage src={image.webformatURL} placeholder={image.previewURL}>
+        {(src, loading) => (
+          <img
+            style={{ opacity: loading ? 0.5 : 1 }}
+            src={src}
+            alt={image.tags}
+            data-image={image.largeImageURL}
+            className={s.image}
+          />
+        )}
+      </ProgressiveImage>
     </li>
   );
 }
